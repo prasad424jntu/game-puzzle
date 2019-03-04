08d74105c20ecde77dd10fc84d1558be69cfaaac
prasad424jntu <prasad424jntu@gmail.com>
2019-03-04 19:56:56 +0800

    Create Position.java

---
+++ src/main/java/commons/Position.java
@@ -0,0 +1,53 @@
+package commons;
+
+import java.io.Serializable;
+
+public class Position implements Serializable{
+	
+	int x;
+	int y;
+	
+	
+	public Position() {
+		
+	}
+	
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Position pair = (Position) o;
+
+        if (x != pair.x) return false;
+        if (y != pair.y) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
+}
